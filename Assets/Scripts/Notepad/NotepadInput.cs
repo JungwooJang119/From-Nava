74c4f5d8ea0890b92eee4a5f815651bb9f3db69f
ChaseOB <chaseobrien128@gmail.com>
2023-01-25 18:05:30 -0500

    boilerplate code wooo

---
+++ Assets/Scripts/Notepad/NotepadInput.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotepadInput : MonoBehaviour
+{
+    //Base class for all notepad input types. 
+}
---
+++ Assets/Scripts/Notepad/NotepadNumpadInput.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotepadNumpadInput : MonoBehaviour
+{
+    //Uses numpad in place of notepad nodes for inputting spells
+}

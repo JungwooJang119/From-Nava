ceaa67a286758e5ea4407644db68515a82a55aa3
Grace-Shao <graceshao203@gmail.com>
2023-01-28 20:57:56 -0500

    created PlayerItemInteraction.cs and a seperate scene for testing

---
+++ Assets/Scripts/Player/PlayerItemInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemInteraction : MonoBehaviour
+{
+    Vector2 range;
+    PlayerController player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //if ()
+        }
+    }
+}

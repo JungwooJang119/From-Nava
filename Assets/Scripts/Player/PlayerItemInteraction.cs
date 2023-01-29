53d1f4a3d36ea466b7da9dd78d6b2fcb4f9ff633
Grace-Shao <graceshao203@gmail.com>
2023-01-28 21:18:19 -0500

    When the user gets close to item, text will pop up

--- Assets/Scripts/Player/PlayerItemInteraction.cs
+++ Assets/Scripts/Player/PlayerItemInteraction.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class PlayerItemInteraction : MonoBehaviour
 {
-    Vector2 range;
-    PlayerController player;
+    [SerializeField] float range;
+    float currentDistance;
+    Vector2 playerPosition;
+    GameObject player;
+
+    /*UI element*/
+    [SerializeField] TMP_Text text;
+
 
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            //if ()
+            playerPosition = player.GetComponent<Transform>().position;
+            currentDistance = (playerPosition - (Vector2)transform.position).magnitude;
+            if (currentDistance < range)
+            {
+                // display UI text
+            }
         }
     }
 }

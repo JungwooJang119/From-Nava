7c07fdee9ccc65f5f84ab2453101f8c6dd77bb2b
Grace-Shao <graceshao203@gmail.com>
2023-02-02 22:43:40 -0500

    Renamed script to ItemIteractionTextPopup, script works
    script located in ui folder.

--- Assets/Scripts/UI/ItemInteractionTextPopup.cs
+++ Assets/Scripts/UI/ItemInteractionTextPopup.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 
-public class PlayerItemInteraction : MonoBehaviour
+public class ItemInteractionTextPopup : MonoBehaviour
 {
-    [SerializeField] GameObject floatingTextPrefab;
-    [SerializeField] GameObject textPopUpLocation;
+    [SerializeField] GameObject floatingTextPrefab;     // text popup
+    [SerializeField] GameObject textPopUpLocation;      // empty text pop up location
     [SerializeField] float secondsToDestroy;
     [SerializeField] float range;
     float currentDistance;
@@ -28,18 +28,29 @@ public class PlayerItemInteraction : MonoBehaviour
             // checks if player is near the object
             playerPosition = player.GetComponent<Transform>().position;
             currentDistance = (playerPosition - (Vector2)transform.position).magnitude;
+            // if the player is in range of the object
             if (currentDistance < range)
             {
-                // display UI text
                 // if floatingTextPrefab has something assigned to it and text hasn't been spawned yet (makes only one clone spawn at a time);
                 if (floatingTextPrefab && isTextInstantiated != true)
                 {
                     isTextInstantiated = true;
+                    // instantiate text
                     GameObject prefab = Instantiate(floatingTextPrefab, textPopUpLocation.transform.position, Quaternion.identity);
+                    // destroy after x seconds
                     Destroy(prefab, secondsToDestroy);
-                    isTextInstantiated = false;
+                    // wait for x seconds before new text can be spawned
+                    StartCoroutine(WaitForSeconds());
                 }
             }
         }
     }
+    IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(secondsToDestroy);
+        // after x seconds, text is NOT instantiated
+        isTextInstantiated = false;
+    }
 }
+
+

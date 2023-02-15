d2c85323dbad67e4abd2f7f272dc1f1a7d86b7df
Elizabeth Liu <esl1350@gmail.com>
2023-02-15 15:40:39 -0500

    wind blast implementation

--- Assets/Scripts/Spells/SpellCastManager.cs
+++ Assets/Scripts/Spells/SpellCastManager.cs
@@ -36,15 +36,16 @@ public class SpellCastManager : MonoBehaviour
 
     private void OnSpellCast(object sender, NotepadLogic.OnSpellCastArgs e)
     {
+        
         if(e.spellType == SpellType.NONE)
         {
             return;
         }
-
+        Vector2 face = new Vector2(PlayerController.Instance.FacingDir.x, PlayerController.Instance.FacingDir.y);
         Spell spell = spellDict[e.spellType];
         
+        spell.CastSpell(face);
         Instantiate(spell, PlayerController.Instance.castPoint.position, Quaternion.identity);
-        spell.CastSpell(PlayerController.Instance.FacingDir);
     }
 
 }
@@ -54,5 +55,6 @@ public enum SpellType {
     FIREBALL,
     CHAIR,
     ICEBALL,
+    WINDBLAST,
     NONE
 }

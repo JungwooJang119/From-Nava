f6a66aec5fc2c89407778a087de7a0208c372653
JungwooJang0911 <jungwoojang2001@gmail.com>
2023-03-25 01:12:16 -0400

    Refined Doors

---
+++ Assets/Door.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor() {
+        animator.SetBool("IsUnlocked", true);
+    }
+}

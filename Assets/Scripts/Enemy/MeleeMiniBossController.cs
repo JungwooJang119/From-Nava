f370517720e16c57d01cf7fb5b25712e72fb1f4a
Cole Sosnowski <cole.sosnowski@outlook.com>
2023-04-05 17:50:25 -0400

    enemy prefab created

--- Assets/Scripts/Enemy/MeleeMiniBossController.cs
+++ Assets/Scripts/Enemy/MeleeMiniBossController.cs
@@ -8,74 +8,88 @@ public class MeleeMiniBossController : MonoBehaviour
     public float speed;
     public Transform player;
     public float minDistance;
-    public float maxDistance;
+    public float engageDistance;
 
-    public GameObject fireball;
-    public float fireballInterval;
-    public float currFireballTime;
-    public bool hasFired;
+    public float attackInterval;
+    public float attackTime;
+    public float currTime;
+
+    private bool attacking;
 
     public bool idle;
 
-    private Vector2 direction;
-    private Vector2 movement;
-
-    public float changeTime;
-    private float lastChangeTime;
-
     private Enemy enemy;
+
+    private GameObject weapon;
+
+    private Vector2 pos1;
+    private Vector2 pos2;
+
+    private Vector3 dir;
 
 
     private void Start()
     {
+        idle = true;
+        attacking = false;
         GetComponent<Rigidbody2D>().gravityScale = 0;
         enemy = GetComponent<Enemy>();
-        lastChangeTime = 0f;
-        NewDirection();
-    }
-
-    private void NewDirection()
-    {
-        direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
-        movement = direction * speed;
+        weapon = transform.GetChild(0).gameObject;
+        weapon.SetActive(false);
+        currTime = 0;
+        pos1 = transform.position + new Vector3(2, 0, 0);
+        pos2 = transform.position + new Vector3(-2, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.eulerAngles.z != 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
+        if (idle)
+        {
+            transform.position = Vector2.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed * 0.1f, 1.0f));
+        }
+        
         if(!enemy.GetPushed()){
-            if (Vector2.Distance(transform.position, player.position) < maxDistance) {
-                if (idle) {
-                    idle = false;
-                }
-                if (currFireballTime >= fireballInterval) {
-                    Instantiate(fireball, transform.position, Quaternion.identity);
-                    currFireballTime = 0;
-                } else {
-                    currFireballTime += Time.deltaTime;
-                }
-
-                if (Vector2.Distance(transform.position, player.position) > minDistance) {
+            if (Vector2.Distance(transform.position, player.position) < engageDistance || !idle)
+            {
+                idle = false;
+                currTime += Time.deltaTime;
+                if (Vector2.Distance(transform.position, player.position) > minDistance && !attacking)
+                {
                     transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
                 }
-            } else {
-                if (!idle) {
-                    idle = true;
-                    NewDirection();
+                if (currTime >= attackInterval)
+                {
+                    dir = player.position - transform.position;
+                    weapon.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+                    attacking = true;
+                    weapon.SetActive(true);
+                    currTime = 0;
                 }
-                if (Time.time - lastChangeTime > changeTime) {
-                    lastChangeTime = Time.time;
-                    NewDirection();
+                if (attacking && (currTime >= attackTime))
+                {
+                    attacking = false;
+                    weapon.SetActive(false);
+                    currTime = 0;
                 }
-                transform.position = new Vector2(transform.position.x + (movement.x * Time.deltaTime), transform.position.y + (movement.y * Time.deltaTime));
             }
         } else {
-            enemy.PushTranslate();
+            //enemy.PushTranslate();
         }
     }
 
-    private void OnCollisionStay2D(UnityEngine.Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        NewDirection();
+        if(collision.gameObject == player.gameObject && collision.GetContact(0).collider.gameObject != this.gameObject)
+        {
+            print("player hit");
+            //player.position = player.position + dir;
+            player.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(2000*dir);
+        }
     }
 }

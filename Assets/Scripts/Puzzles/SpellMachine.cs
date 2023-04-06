25850d06097d427b237a5da83cfe3ea073697860
Carlos Suarez Barrera <112778286+Carlijavier5@users.noreply.github.com>
2023-04-05 23:15:10 -0400

    Pressure plate & transition fixes (#38)
    * creating baseline
    
    * idk why its asking for this
    
    * PPlate and Transition Fixes
    
    * Update M3.unity
    
    * fixing roomcontrola2.cs
    
    * fix for the unintentional bounciness of laser beam
    
    * small fixes
    
    * SpellCasting Machine
    
    * improving code arch

---
+++ Assets/Scripts/Puzzles/SpellMachine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellMachine : MonoBehaviour
+{
+    [SerializeField] Spell spell;
+    [SerializeField] float timeBetweenCasts = 5;
+	[SerializeField] bool active;
+    private float time = 1;
+
+    void Update() {
+        if (active) {
+			if (time <= 0) {
+				spell.CastSpell(Vector2.right);
+				Instantiate(spell, transform.position, Quaternion.identity);
+				time = timeBetweenCasts;
+			}
+			time -= Time.deltaTime;
+		}
+	}
+	public void Activate() {
+		active = true;
+	}
+	public void Deactivate() {
+		active = false;
+	}
+}

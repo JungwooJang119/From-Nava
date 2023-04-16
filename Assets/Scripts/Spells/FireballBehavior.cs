22af6f555f3b543c1cba129b09a74d86a6657c35
Carlijavier5 <carlijavier5@gmail.com>
2023-04-15 21:12:05 -0400

    Spell initialization and cleaner code

--- Assets/Scripts/Spells/FireballBehavior.cs
+++ Assets/Scripts/Spells/FireballBehavior.cs
@@ -3,54 +3,142 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Modifies the default behavior of Fireball. Most spells follow the same pattern:
+// Initialization, Lifetime, Death (just like your average human being), hence,
+// some methods may be similar/identical to other spell's.
+
 public class FireballBehavior : MonoBehaviour {
-    [SerializeField] private SpellScriptObj spellData;
+    [SerializeField] private SpellScriptObj spellData;  // Fireball spell data from the SO;
+
+    // Simple state machine;
+    private enum State {
+        Start,
+        Lifetime,
+        End,
+        Done,
+    } private State state;
 
     private Spell spellScript;          // Reference for Spell.cs;
-    private float deathTimer;           // Timer to account for the spell lifetime;
-    private float scaleFactor;          // Variable to control size decrease;
+	private Transform castPoint;        // Transform the spell will be glued to until brrrrr;
+
+	private float deathTimer;           // Timer to account for the spell lifetime;
+
+    private float scaleFactor;          // Variables to control size;
+    private float maxScaleFactor;
+
     private ParticleSystem parSystem;   // Reference to the particle system (child);
-    private bool awaitForDeath;         // Lazy boolean, sad excuse of a state machine;
+
     private UnityEngine.Rendering.Universal.Light2D[] lightList; // Array to store light references;
+    private float[] lightBounds;        // Target outer radii of light sources;
+    private float lightRate = 0.075f;   // Rate at which the light will change;
 
     // Did you know start is called before the first frame update? :V
     void Start() {
-        // Suscribe to the OnDestroy event from Spell.cs
+        // Suscribe to the OnDestroy event from Spell.cs;
         spellScript = GetComponent<Spell>();
         spellScript.OnSpellDestroy += Spell_OnSpellDestroy;
-        // Variables to manage the particle system and light source
+        
+        // Variables to manage the particle system;
         parSystem = GetComponentInChildren<ParticleSystem>();
-        scaleFactor = transform.localScale.x;
+        var ps = parSystem.emission;
+        ps.enabled = false;
+
+        // Variables to manage the scale;
+        maxScaleFactor = transform.localScale.x;
+        scaleFactor = 0;
+        ChangeSize(0f);
+
+        // Variables to manage the lights;
         lightList = GetComponentsInChildren<UnityEngine.Rendering.Universal.Light2D>();
-    }
+        lightBounds = new float[lightList.Length];
+        for (int i = 0; i < lightList.Length; i++) {
+			lightBounds[i] = lightList[i].pointLightOuterRadius;
+            lightList[i].pointLightOuterRadius = 0;
+		}
+
+		// ALERT: Change if castPoint is no longer public in PlayerController.cs;
+		castPoint = PlayerController.Instance.castPoint;
+	}
 
     // Wait until the spell's life runs out. If it didn't collide, reduce the scale of the fireball sprite
     // and reduce the radius of the light source to 0. Then, yeet all the unnecesary components and set up a
     // small particle burst for visual flavor. The spell shall die when all particles run out ;-;
     void Update() {
-        if (deathTimer < spellData.lifetime) {
-            deathTimer += Time.deltaTime;
-        }
-        else if (!awaitForDeath) {
-			ModifyLight(-0.075f);
-			if (scaleFactor > 0) {
-                ReduceSize(10f);
-            }
-            else {
-				if (spellScript != null) {
-                    CleanUp();
-                    GenerateBurst(1f, 100f);
-                } else {
-                    GenerateBurst(0.75f, 150f);
-                }
-                awaitForDeath = true;
-            }
-        }
-        else {
-			ModifyLight(-0.075f);
-			parSystem.Stop();
-        }
+        switch (state) {
+
+            case State.Start:
+                // Stay on cast point until release;
+                transform.position = castPoint.position;
+                // Deactivate spell on start;
+				if (spellScript != null && spellScript.enabled) {
+					ToggleComponents();
+				}
+                // Expand light source;
+                ModifyLight(lightRate);
+                // Scale up the sprite;
+				if (scaleFactor < maxScaleFactor) {
+					ChangeSize(7.5f);
+				} else {
+                    // Enable emissions;
+					var ps = parSystem.emission;
+					ps.enabled = true;
+                    // Reactivate components;
+					ToggleComponents();
+                    state = State.Lifetime;
+				}
+                break;
+
+            case State.Lifetime:
+				if (deathTimer < spellData.lifetime) {
+                    // Wait for lifetime;
+					deathTimer += Time.deltaTime;
+                    // Continue expanding light source;
+                    ModifyLight(lightRate);
+					// Small scale oscillation effect. Too lazy to make it cleaner :(
+					scaleFactor = Mathf.Sin(Time.time * 7.5f) * maxScaleFactor / 20f + maxScaleFactor;
+					transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+					break;
+				} else {
+                    // Reset radii bounds for light source;
+                    for (int i = 0; i < lightBounds.Length; i++) { lightBounds[i] = 0; }
+                    state = State.End;
+				}
+                break;
+
+            case State.End:
+                // Dim the light sources;
+				ModifyLight(-lightRate);
+                // Scale down the sprite;
+				if (scaleFactor > 0) {
+					ChangeSize(-10f);
+				} else {
+                    // Stop spell and generate particles;
+					if (spellScript != null) {
+						CleanUp();
+						GenerateBurst(1f, 100f);
+					} else {
+						GenerateBurst(0.75f, 150f);
+					}
+					state = State.Done;
+				}
+                break;
+            case State.Done:
+                // Stop emissions;
+				parSystem.Stop();
+                // Continue dimming the light sources;
+				ModifyLight(-lightRate);
+                break;
+		}
     }
+
+    // Method to "disable" functionality at the start;
+    private void ToggleComponents() {
+		spellScript.enabled = spellScript.enabled ? false : true;
+        var rb = GetComponent<Rigidbody2D>();
+		rb.simulated = rb.simulated ? false : true;
+        var coll = GetComponent<Collider2D>();
+		coll.enabled = coll.enabled ? false : true;
+	}
 
     // Method to take away functionality after the lifetime has been exhausted;
     private void CleanUp() {
@@ -65,18 +153,25 @@ public class FireballBehavior : MonoBehaviour {
         CleanUp();
     }
 
-    private void ReduceSize(float reductionMultiplier) {
-        scaleFactor -= Time.deltaTime * reductionMultiplier;
+    // Method to change the size of the spell sprite;
+    private void ChangeSize(float sizeMultiplier) {
+        scaleFactor += Time.deltaTime * sizeMultiplier;
         transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
     }
 
-    private void ModifyLight(float rate) {
-        foreach (UnityEngine.Rendering.Universal.Light2D light in lightList) {
-            light.pointLightOuterRadius = Mathf.Max(0, light.pointLightOuterRadius + rate);
+	// Method to modify the light sources attached to the spell;
+	private void ModifyLight(float rate) {
+        for (int i = 0; i < lightList.Length; i++) {
+            if (lightBounds[0] > 0) {
+				lightList[i].pointLightOuterRadius = Mathf.Min(lightBounds[i], lightList[i].pointLightOuterRadius + rate);
+			} else {
+				lightList[i].pointLightOuterRadius = Mathf.Max(lightBounds[i], lightList[i].pointLightOuterRadius + rate);
+			}
         }
     }
 
-    private void GenerateBurst(float particleLifetime, float particleSpeed) {
+	// Method to generate a particle burst;
+	private void GenerateBurst(float particleLifetime, float particleSpeed) {
         var parMainSystem = parSystem.main;
         parMainSystem.startLifetime = particleLifetime;
         parMainSystem.startSpeed = particleSpeed;
--- Assets/Scripts/Spells/WindblastBehavior.cs
+++ Assets/Scripts/Spells/WindblastBehavior.cs
@@ -2,25 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindblastBehavior : MonoBehaviour
-{
+// Modifies the default behavior of Windblast. Most spells follow the same pattern:
+// Initialization, Lifetime, Death (just like your average dog), hence,
+// some methods may be similar/identical to other spell's.
+// Due to the fast nature of the Windblast, it does not have an initialization phase;
+
+public class WindblastBehavior : MonoBehaviour {
+
 	[SerializeField] private SpellScriptObj spellData;  // Windspell data from the SO;
 
     [SerializeField] public float pushDist = 0f;
     [SerializeField] public float pushSpd = 1f;
 
-	private Spell spell;                    // Reference to spell component in this gameObject;
-    private ParticleSystem[] parSystems;    // Children Particle Systems. Reverb on 0, Trail on 1;
+	// Simple state machine;
+	private enum State {
+		Lifetime,
+		End,
+		Done,
+	}
+	private State state;
 
-	private float xScaleFactor;
-	private float yScaleFactor;
+	private Spell spell;                    // Reference to spell component in this gameObject;
 
 	private float deathTimer;               // Timer to account for the spell lifetime;
-	private bool awaitForDeath;             // Lazy boolean, sad excuse for a state machine;
+
+	private float xScaleFactor;				// Variables to control scale;
+	private float yScaleFactor;
+
+	private ParticleSystem[] parSystems;    // Children Particle Systems. Reverb on 0, Trail on 1;
 
 	// Have you heard of start, the method called before the first frame of Update? :O
 	void Start() {
-        spell = GetComponent<Spell>();
+		// Suscribe to the OnDestroy event from Spell.cs
+		spell = GetComponent<Spell>();
 		spell.OnSpellDestroy += Spell_OnSpellDestroy;
 
 		parSystems = GetComponentsInChildren<ParticleSystem>();
@@ -33,7 +47,8 @@ public class WindblastBehavior : MonoBehaviour
         // Set the rotation of the reverb particles to the right rotation;
         var mainParReverb = parSystems[0].main;
         mainParReverb.startRotation = Mathf.Deg2Rad * (360 - transform.rotation.eulerAngles.z);
-		
+
+		// Variables to manage the scale;
 		xScaleFactor = transform.localScale.x;
 		yScaleFactor = transform.localScale.y;
 	}
@@ -42,28 +57,44 @@ public class WindblastBehavior : MonoBehaviour
 	// If it does collider, reduce the y-scale instead. Then, yeet all the unnecesary components and
 	// set up a small particle burst for visual flavor. The spell shall die when all particles run out ;-;
 	void Update() {
-		if (deathTimer < spellData.lifetime) {
-			deathTimer += Time.deltaTime;
-		}
-		else if (!awaitForDeath) {
-			if (yScaleFactor > 0) {
-				ReduceScale(7.5f);
-			} else {
-				var noiseParTrail = parSystems[1].noise;
-				noiseParTrail.enabled = true;
-				if (spell != null) {
-					parSystems[1].gameObject.transform.Rotate(new Vector3(0,0,180f));
-					CleanUp();
-					GenerateBurst(0.75f, 1f);
-				} else {
-					GenerateBurst(0.75f, 1f);
+		switch (state) {
+
+			case State.Lifetime:
+				if (deathTimer < spellData.lifetime) {
+					// Wait for lifetime;
+					deathTimer += Time.deltaTime;
+					break;
 				}
-				awaitForDeath = true;
-			}
-		}
-		else {
-			parSystems[0].Stop();
-			parSystems[1].Stop();
+				else {
+					state = State.End;
+				}
+				break;
+
+			case State.End:
+				// Scale down the sprite;
+				if (yScaleFactor > 0) {
+					ChangeYScale(-7.5f);
+				} else {
+					// Add noise to trail for the dissipation burst;
+					var noiseParTrail = parSystems[1].noise;
+					noiseParTrail.enabled = true;
+					// Change the angle of the particle generator and stop spell;
+					if (spell != null) {
+						parSystems[1].gameObject.transform.Rotate(new Vector3(0, 0, 180f));
+						CleanUp();
+						GenerateBurst(0.75f, 1f);
+					} else {
+						GenerateBurst(0.75f, 1f);
+					}
+					state = State.Done;
+				}
+				break;
+
+			case State.Done:
+				// Stop emissions;
+				parSystems[0].Stop();
+				parSystems[1].Stop();
+				break;
 		}
 	}
 
@@ -80,16 +111,18 @@ public class WindblastBehavior : MonoBehaviour
 		CleanUp();
 	}
 
-	private void ReduceScale(float reductionMultiplier) {
-		yScaleFactor = Mathf.Max(0, yScaleFactor - Time.deltaTime * reductionMultiplier);
+	// Method to change the y-scale of the sprite;
+	private void ChangeYScale(float reductionMultiplier) {
+		yScaleFactor = Mathf.Max(0, yScaleFactor + Time.deltaTime * reductionMultiplier);
 		transform.localScale = new Vector3(xScaleFactor, yScaleFactor, 1f);
 	}
 
+	// Method to generate a particle burst;
 	private void GenerateBurst(float particleLifetime, float particleSpeed) {
 		var mainParTrail = parSystems[1].main;
 		mainParTrail.startLifetime = particleLifetime;
 		mainParTrail.startSpeed = particleSpeed;
-		parSystems[1].emission.SetBursts(new ParticleSystem.Burst[] { new ParticleSystem.Burst(0f, 60) });
+		parSystems[1].emission.SetBursts(new ParticleSystem.Burst[] { new ParticleSystem.Burst(0f, 120) });
 		parSystems[1].Stop();
 		parSystems[1].Play();
 		Destroy(this.gameObject, parSystems[1].main.startLifetime.constant);
@@ -99,7 +132,7 @@ public class WindblastBehavior : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy")) {
             other.gameObject.GetComponent<Enemy>().Push(spell.direction, pushDist, pushSpd);
-        } else if (other.gameObject.CompareTag("Mirror")) {                                     //Mirror push adition, by Carlos.
+        } else if (other.gameObject.CompareTag("Mirror")) {
             other.gameObject.GetComponent<Mirror>().Push(spell.direction, pushDist, pushSpd);   //Mirror script will mitigate push values.
         }
         if(other.gameObject.CompareTag("Fan")) {

199e91ac15a7cbebc528ccafbdabe7b8a311935f
Elizabeth Liu <40213724+esl1350@users.noreply.github.com>
2023-04-18 18:19:40 -0400

    Spike Implementation (#52)
    * wow that was very unfortunate
    
    * guess who didn't normalize their vectors
    
    * update spike prefab

---
+++ Assets/Scripts/SpikeBehavior.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBehavior : MonoBehaviour
+{
+    [SerializeField] private int dmg = 1;
+    [SerializeField] private float knockbackSpd = 3f;
+    [SerializeField] private float knockbackDist = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {        
+        if (other.gameObject.CompareTag("Enemy")) { 
+            Vector2 avgCollision = new Vector2(0, 0);
+
+            for (int i = 0; i < other.contactCount; i++) {
+                avgCollision += other.GetContact(i).point;
+            }
+            avgCollision /= other.contactCount;
+
+            Vector3 contact = new Vector3(avgCollision.x, avgCollision.y, other.transform.position.z);
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 pushDir = Vector3.Normalize(other.transform.position - contact);
+            other.gameObject.GetComponent<Enemy>().Push(pushDir, knockbackDist, knockbackSpd);
+            other.gameObject.GetComponent<Enemy>().TakeDamage((float)dmg);
+        } else if (other.gameObject.CompareTag("Player")) {
+            Vector2 avgCollision = new Vector2(0, 0);
+
+            for (int i = 0; i < other.contactCount; i++) {
+                avgCollision += other.GetContact(i).point;
+            }
+            avgCollision /= other.contactCount;
+
+            Vector3 contact = new Vector3(avgCollision.x, avgCollision.y, other.transform.position.z);
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 pushDir = Vector3.Normalize(other.transform.position - contact);
+            Debug.Log(pushDir);
+
+            other.gameObject.GetComponent<PlayerController>().Push(pushDir, knockbackDist, knockbackSpd);
+            other.gameObject.GetComponent<PlayerController>().TakeDamage(dmg);
+
+        }
+    }
+}

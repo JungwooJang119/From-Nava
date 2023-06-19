bddb78c8f7746b938e7f0479f6f62b1dc27c7b2a
Carlijavier5 <carlijavier5@gmail.com>
2023-06-19 18:06:18 -0400

    Patcher Update to support the 109th commit
    Prefabs... Need... Serialization Support...
    2022.3... Do I miss you? T-T

--- Assets/LightUpdate/LightUpdatePatcher.cs
+++ Assets/LightUpdate/LightUpdatePatcher.cs
@@ -14,18 +14,27 @@ public class LightUpdatePatcher : MonoBehaviour {
         PrefabUtility.UnpackPrefabInstance(GameObject.Find("RoomLights"), PrefabUnpackMode.Completely, InteractionMode.UserAction);
         // Update RoomControl variables
         var roomControl = GameObject.Find("RoomControl").transform;
-        QueueFind(roomControl, "A2 - 1").GetComponent<RoomControlA2>().SetRoomCode(RoomCode.A2_2);
-        QueueFind(roomControl, "A2 - 2").GetComponent<RoomControlA2>().SetRoomCode(RoomCode.A2_3);
-        QueueFind(roomControl, "B3 - 1").GetComponent<RoomControlA3>().SetRoomCode(RoomCode.B3_2);
-        QueueFind(roomControl, "B3 - 2").GetComponent<RoomControlA3>().SetRoomCode(RoomCode.B3_3);
+        var tempComponent1 = QueueFind(roomControl, "A2 - 1").GetComponent<RoomControlA2>();
+        tempComponent1.SetRoomCode(RoomCode.A2_2);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempComponent1);
+        tempComponent1 = QueueFind(roomControl, "A2 - 2").GetComponent<RoomControlA2>();
+        tempComponent1.SetRoomCode(RoomCode.A2_3);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempComponent1);
+        var tempComponent2 = QueueFind(roomControl, "B3 - 1").GetComponent<RoomControlA3>();
+        tempComponent2.cameraTarget = tempComponent2.door;
+        tempComponent2.SetRoomCode(RoomCode.B3_2);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempComponent2);
+        tempComponent2 = QueueFind(roomControl, "B3 - 2").GetComponent<RoomControlA3>();
+        tempComponent2.SetRoomCode(RoomCode.B3_3);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempComponent2);
         // Update Terminal variables
         var tempTerminal = GameObject.Find("C2 - 1").transform.Find("Terminal");
-        PrefabUtility.UnpackPrefabInstance(tempTerminal.gameObject, PrefabUnpackMode.Completely, InteractionMode.UserAction);
         tempTerminal.GetComponent<LaserTerminal>().SetRoomCode(RoomCode.C2_2);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempTerminal.GetComponent<LaserTerminal>());
         tempTerminal = GameObject.Find("C2 - 2").transform.Find("Terminal");
-        PrefabUtility.UnpackPrefabInstance(tempTerminal.gameObject, PrefabUnpackMode.Completely, InteractionMode.UserAction);
         tempTerminal.GetComponent<LaserTerminal>().SetRoomCode(RoomCode.C2_3);
-        EditorUtility.DisplayDialog("Patch Complete", "Scene Patch Successful! (Probably)", ":happypiplup:");
+        PrefabUtility.RecordPrefabInstancePropertyModifications(tempTerminal.GetComponent<LaserTerminal>());
+        EditorUtility.DisplayDialog("Patch Complete", "Scene Patch Successful! (Probably)\nYou can now delete the LightUpdate Folder under Assets. We don't need it anymore ;-;", ":happypiplup:");
         StartCoroutine(WaitForRoomLights());
     }
 
@@ -42,8 +51,8 @@ public class LightUpdatePatcher : MonoBehaviour {
 
     private IEnumerator WaitForRoomLights() {
         while (!GameObject.Find("RoomLights").GetComponent<RoomLights>()) yield return null;
-        PrefabUtility.UnpackPrefabInstance(GameObject.Find("ReferenceSingleton"), PrefabUnpackMode.Completely, InteractionMode.UserAction);
         GameObject.Find("ReferenceSingleton").GetComponent<ReferenceSingleton>().roomLights = GameObject.Find("RoomLights").GetComponent<RoomLights>();
+        PrefabUtility.RecordPrefabInstancePropertyModifications(GameObject.Find("ReferenceSingleton").GetComponent<ReferenceSingleton>());
         DestroyImmediate(gameObject);
     }
 }
@@ -54,6 +63,8 @@ public class LightUpdatePatcherEditor : Editor {
     public override void OnInspectorGUI() {
         LightUpdatePatcher patchScript = (LightUpdatePatcher) target;
 
+        EditorGUILayout.LabelField("Press This Button To Patch the M3 Scene. I wrote this line by line so...");
+        EditorGUILayout.LabelField("Totally safe yk (probably, yes >.>)");
         if (GUILayout.Button("Patch Scene")) {
             patchScript.PatchGame();
         }

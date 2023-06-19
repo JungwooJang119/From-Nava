cb0a14b2579d0be5935f4fe6c2989bcffade6091
Carlijavier5 <carlijavier5@gmail.com>
2023-06-19 13:03:31 -0400

    Part II - Basic Version Set-Up
    Includes Freeform Lights for each room + The basic (T-T) version of the room light-up mechanic.

--- Assets/Scripts/Lights/RecedingLight.cs
+++ Assets/Scripts/Lights/RecedingLight.cs
@@ -1,3 +1,48 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class RecedingLight : MonoBehaviour {
+    [SerializeField] private float propagationSpeed = 1f;
+    private new Light2D light;
+    private float intensity;
+    private bool propagate;
+
+    private void Start() {
+        light = GetComponent<Light2D>();
+        intensity = light.intensity;
+        light.intensity = 0;
+    }
+
+    private void Update() {
+        if (propagate) light.intensity = Approach(light.intensity, intensity, Time.deltaTime * propagationSpeed);
+        if (Input.GetKeyDown("u")) Propagate();
+    }
+
+    public void Propagate() {
+        propagate = true;
+    }
+
+    // Approach one value to another;
+    private float Approach(float currentValue, float targetValue, float rate) {
+        rate = Mathf.Abs(rate);
+        if (currentValue < targetValue) {
+            currentValue += rate;
+            if (currentValue > targetValue) return targetValue;
+        } else {
+            currentValue -= rate;
+            if (currentValue < targetValue) return targetValue;
+        } return currentValue;
+    }
+}
+
+
+
+/// 
+// Alternative Version
+///
+
+/*
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -8,94 +53,134 @@ using UnityEditor;
 public class RecedingLight : MonoBehaviour {
 
     [SerializeField] private float propagationSpeed = 1f;
+
+    [Tooltip("Starting positions of the light. The order of the elements matters;")]
+    [SerializeField] private int[] startCoordinates;
+
+    [Tooltip("Vertex groups to be propagated by specific vertices. The internal and external order of vertices matters;")]
+    [SerializeField] private VertexGroup[] vertexGroups;
+
     [SerializeField] private LightVertex[] vertices;
+    [SerializeField] private List<LightVertex> activeVertices;
     private new Light2D light;
     private bool propagate;
 
     // Start is called before the first frame update
     void Start() {
         light = GetComponentInChildren<Light2D>();
-        for (int i = 0; i < light.shapePath.Length; i++) {
-            if (vertices[i].staticVertex) {
-                light.shapePath[i] = vertices[i].transform.localPosition;
-            } else {
-                light.shapePath[i] = Vector3.zero;
-            }
+        //light.intensity = 0;
+        activeVertices = new List<LightVertex>();
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i].position = vertices[i].destination;
         }
     }
 
     // Update is called once per frame
     void Update() {
-        if (propagate) {
-            for (int i = 0; i < light.shapePath.Length; i++) {
-                var target = vertices[i].transform.localPosition;
-                if (vertices[i].path.Count > 0) target = vertices[i].path[0].localPosition;
-                light.shapePath[i] = new Vector2(Approach(light.shapePath[i].x, target.x, (target.x / transform.position.x) * propagationSpeed * Time.deltaTime),
-                                                 Approach(light.shapePath[i].y, target.y, (target.y / transform.position.y) * propagationSpeed * Time.deltaTime));
-                if (vertices[i].path != null && vertices[i].path.Count > 0 && light.shapePath[i] == target) vertices[i].path.RemoveAt(0);
-                //Debug.Log(light.shapePath[i] + " vertex " + target + " speed " + (transform.position.y - target.y) * propagationSpeed);
-            }
+        var lightShape = new List<Vector3>();
+        for (int i = 0; i < activeVertices.Count; i++) {
+            var vertex = activeVertices[i];
+            if (vertex.active) {
+                activeVertices[i].position = new Vector3(Approach(vertex.position.x, vertex.destination.x, 
+                                                         vertex.destination.x / transform.localPosition.x * propagationSpeed * Time.deltaTime),
+                                                         Approach(vertex.position.y, vertex.destination.y,
+                                                         vertex.destination.y / transform.localPosition.y * propagationSpeed * Time.deltaTime));
+                if (vertex.position == vertex.destination) {
+                    vertex.active = false;
+                    if (vertex.propagateGroup > 0) { PropagateGroup(vertexGroups[vertex.propagateGroup - 1], vertex.destination); }
+                }
+            } lightShape.Add(activeVertices[i].position);
+        } SetShapePath(lightShape.ToArray());
+        if (Input.GetKeyUp("u") && !propagate) { Propagate(); propagate = true; }
+    }
+
+    public void Propagate() {
+        foreach (int vertexIndex in startCoordinates) {
+            var i = vertexIndex - 1;
+            activeVertices.Add(vertices[i]);
         }
-        if (Input.GetKey("u")) propagate = true;
+    }
+
+    public void PropagateGroup(VertexGroup group, Vector3 startPosition) {
+        for (int i = group.start - 1; i < group.end; i++) {
+            vertices[i].position = startPosition;
+            activeVertices.Add(vertices[i]);
+        } activeVertices.Sort((v1, v2) => int.Parse(v1.name.Split(" ")[1]).CompareTo(int.Parse(v2.name.Split(" ")[1])));
     }
 
     public void PopulateLightController() {
         if (light == null) light = GetComponentInChildren<Light2D>();
-        if (vertices != null) foreach (LightVertex v in vertices) { if (v != null && v.transform != null) DestroyImmediate(v.transform.gameObject); }
         vertices = new LightVertex[light.shapePath.Length];
         for (int i = 0; i < light.shapePath.Length; i++) {
-            vertices[i] = new LightVertex();
-            vertices[i].name += i + 1;
-            var posGO = new GameObject("Light " + (i + 1));
-            posGO.transform.SetParent(transform);
-            posGO.transform.localPosition = light.shapePath[i];
-            vertices[i].transform = posGO.transform;
+            vertices[i] = new LightVertex(light.shapePath[i]);
+            vertices[i].name += (i + 1);
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        for (int i = 0; i < vertices.Length; i++) {
+            var diskRadius = 0.25f;
+            if (Camera.current != null) { diskRadius = 0.05f * Camera.current.orthographicSize; }
+            Handles.DrawSolidDisc(Vector3.zero + transform.position + vertices[i].destination, Vector3.forward, diskRadius);
+
+            var labelStyle = new GUIStyle();
+            labelStyle.normal.textColor = Color.red;
+            labelStyle.alignment = TextAnchor.MiddleCenter;
+            Handles.Label(Vector3.zero + transform.position + vertices[i].destination, (i + 1).ToString(), labelStyle);
         }
     }
 
     public void ResetShape() {
-		if (light == null) light = GetComponentInChildren<Light2D>();
+        if (light == null) light = GetComponentInChildren<Light2D>();
         var vertexArray = new Vector3[vertices.Length];
         for (int i = 0; i < vertices.Length; i++) {
-            vertexArray[i] = vertices[i].transform.localPosition;
-		} SetShapePath(vertexArray);
+            vertexArray[i] = vertices[i].destination;
+        } SetShapePath(vertexArray);
     }
 
     public void ClearLightController() {
-        if (light == null) light = GetComponentInChildren<Light2D>();
-        for (int i = 0; i < vertices.Length; i++) {
-            vertices[i].transform.localPosition = transform.localPosition;
-        }
+        // To-Do;
     }
 
-	// Set shapePath of freeform light
-	private void SetShapePath(Vector3[] path) {
-		var field = light.GetType().GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
-		field?.SetValue(light, path);
-	}
+    // Set shapePath of freeform light
+    private void SetShapePath(Vector3[] path) {
+        var field = light.GetType().GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
+        field?.SetValue(light, path);
+    }
 
     // Approach one value to another;
-	private float Approach(float currentValue, float targetValue, float rate) {
-		rate = Mathf.Abs(rate);
-		if (currentValue < targetValue) {
-			currentValue += rate;
-			if (currentValue > targetValue) return targetValue;
-		} else {
-			currentValue -= rate;
-			if (currentValue < targetValue) return targetValue;
-		} return currentValue;
-	}
+    private float Approach(float currentValue, float targetValue, float rate) {
+        rate = Mathf.Abs(rate);
+        if (currentValue < targetValue) {
+            currentValue += rate;
+            if (currentValue > targetValue) return targetValue;
+        } else {
+            currentValue -= rate;
+            if (currentValue < targetValue) return targetValue;
+        } return currentValue;
+    }
 }
 
-[System.Serializable]
+[Serializable]
 public class LightVertex {
     [HideInInspector] public string name = "Vertex ";
-    [Tooltip("Whether the light vertex starts at the target position;")]
-    public bool staticVertex;
-    [Tooltip("Position of the vertex in worldspace;")]
-    public Transform transform;
-    [Tooltip("Vertices to traverse before moving towards the final position. Use indexes;")]
-    public List<Transform> path;
+    [HideInInspector] public Vector3 position;
+    [HideInInspector] public Vector3 destination;
+    public int propagateGroup;
+    [HideInInspector] public bool active = true;
+
+    public LightVertex(Vector3 destination) {
+        this.destination = destination;
+    }
+}
+
+[Serializable]
+public class VertexGroup {
+    [HideInInspector] public string name = "Group ";
+    [Tooltip("Range of vertices belonging to the group;")]
+    public int start; public int end;
+    // Manipulated at runtime;
+    private Vector3[] destinations;
 }
 
 [CustomEditor(typeof(RecedingLight))]
@@ -104,12 +189,13 @@ public class LightInspectorUtilEditor : Editor {
         base.OnInspectorGUI();
         RecedingLight lightScript = (RecedingLight) target;
 
-        if (GUILayout.Button("Populate Coordinates")) {
+        if (GUILayout.Button("Generate Vertices")) {
             lightScript.PopulateLightController();
-        } if (GUILayout.Button("Reset Shape")) {
+        } if (GUILayout.Button("Restore Shape")) {
             lightScript.ResetShape();
-        } if (GUILayout.Button("Clear Coordinates")) {
+        } if (GUILayout.Button("Clear Vertices")) {
             lightScript.ClearLightController();
         }
     }
 }
+*/

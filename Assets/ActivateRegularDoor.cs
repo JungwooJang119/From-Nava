3f594af81d33e9d461e6dc477d8a8e38738ff8d2
Jungwoo <jjang82@gatech.edu>
2024-05-26 09:42:53 -0400

    S4 door

--- Assets/ActivateRegularDoor.cs
+++ Assets/ActivateRegularDoor.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class ActivateRegularDoor : MonoBehaviour
 {
     public GameObject door;
+    public bool isToS4;
     
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player") {
             door.GetComponent<Door>().OpenDoor();
-            Destroy(this.gameObject);
+            if (!isToS4) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sent each frame where another object is within a trigger collider
+    /// attached to this object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && !isToS4) {
+            door.GetComponent<Door>().OpenDoor();
         }
     }
 }
